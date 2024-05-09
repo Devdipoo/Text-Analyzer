8cc1f54a7e496920ffffe69f0cfe790bf2e197de
Devdipoo <emiolaajileye@gmail.com git config --global user.name Devdipoogit config --global user.email emiolaajileye@gmail.com>
2024-05-09 10:30:02 +0100

    Add logic for bolded words

--- js/script.js
+++ js/script.js
@@ -51,24 +51,24 @@ function wordCounter(text) {
     
   
 
-  function boldPassage(word, text) {
-    if (noInputtedWord(word, text)) {
-      return "";
-    }
-    let htmlString = "<p>";
-    let textArray = text.split(" ");
-    textArray.forEach(function(element, index) {
-      if (element.toLowerCase().includes(word.toLowerCase())) {
-        htmlString = htmlString.concat("<b>" + element + "</b>");
-      } else {
-        htmlString = htmlString.concat(element);
-      }
-      if (index !== (textArray.length - 1)) {
-        htmlString = htmlString.concat(" ");
-      }
-    });
-    return htmlString + "</p>";
-  }
+  // function boldPassage(word, text) {
+  //   if (noInputtedWord(word, text)) {
+  //     return "";
+  //   }
+  //   let htmlString = "<p>";
+  //   let textArray = text.split(" ");
+  //   textArray.forEach(function(element, index) {
+  //     if (element.toLowerCase().includes(word.toLowerCase())) {
+  //       htmlString = htmlString.concat("<b>" + element + "</b>");
+  //     } else {
+  //       htmlString = htmlString.concat(element);
+  //     }
+  //     if (index !== (textArray.length - 1)) {
+  //       htmlString = htmlString.concat(" ");
+  //     }
+  //   });
+  //   return htmlString + "</p>";
+  // }
 
   function threeMostCommonWords(sentence) {
     if (noInputtedWord(sentence)) {
